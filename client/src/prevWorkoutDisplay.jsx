3ebe318370b8cd01d1e1b20b590b628615eb37b6
Tyler Brown <brown.tylerfrances@gmail.com>
2025-04-14 10:56:37 -0400

    Working on Previous Workouts Front End

--- client/src/prevWorkoutDisplay.jsx
+++ client/src/prevWorkoutDisplay.jsx
@@ -1,9 +1,16 @@
 // prevWorkoutDisplay.jsx
 import './prevWorkoutDisplay.css';
 
-export default function WorkoutDisplay() {
+export default function WorkoutDisplay({ title, date }) {
     return (
       <div className={`workout-display`}>
+        <div className="workout-display-top-bar">
+            <div className="top-bar-left">{date}</div>
+            <div className="top-bar-center">{title}</div>
+            <button className="edit-button">
+                <img src="/edit.svg" alt="Edit" className="edit-svg-icon" />
+            </button>
+        </div>
       </div>
     );
   }
